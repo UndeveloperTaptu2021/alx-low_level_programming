e038d250001abb1d79a557f6b029f53f11c5811c
Abdul Qoyyum <oriolaaq@gmail.com>
2022-03-11 13:29:35 -0800

    variables

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h> 
+
+/**
+ *  main - the code starting point 
+ *  
+ *  Description : the code to print whether positive or negative vriable
+ * Return: returns after sucessful execution
+ * */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+
+	return (0);
+}
