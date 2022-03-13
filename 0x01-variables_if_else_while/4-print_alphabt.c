9e55e3139ae0d6806a32ec599235a45ce85e9da0
Abdul Qoyyum <oriolaaq@gmail.com>
2022-03-13 10:27:09 -0700

    print lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` to print all letters but the letter 'q' and 'e'.
+ * Return: 0
+ */
+int main()
+{
+     char alphabet = 'a';
+     while (alphabet <= 'z') {
+     
+     if (alphabet !='e' && alphabet !='q')
+     {
+       putchar(alphabet);
+     }
+     alphabet++ ;
+     
+     }
+    putchar('\n');
+     
+
+
+return 0;
+}
